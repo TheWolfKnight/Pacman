4a1479a410326cf3ad3a6d3f6e8c433480344bd2
Philip Knudsen <philipalexkndusen@gmail.com>
2023-02-19 11:17:35 +0100

    removed unused parts

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
 ﻿
-using Pacman.Src.Entity;
-using Pacman.Src.Enemy;
-
 namespace Pacman;
 
 public class Program {
+
+  public static char[,] Board = new char[,] {
+  };
+
   static void Main(string[] args) {
+
+    while (true) {}
+
   }
+
+  static void Setup() {
+    throw new NotImplementedException();
+  }
+
 }
